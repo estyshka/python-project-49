5aea5087b42efa9d2a91d98b94b8e6a4136e8c0b
estyshka <titovanadya27@gmail.com>
2025-04-08 14:15:15 +0300

    step 3: checking mistakes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
-from main import main
+from brain_games.scripts.main import greet
+from brain_games.cli import welcome_user
+
+def main():
+    greet()
 
 
 if __name__ == "__main__":
     main()
+welcome_user()
---
+++ brain_games/scripts/main.py
@@ -0,0 +1,6 @@
+def greet():
+    print("Welcome to the Brain Games!")
+
+
+
+
